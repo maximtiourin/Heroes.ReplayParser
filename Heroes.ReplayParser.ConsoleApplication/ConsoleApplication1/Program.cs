4e84af2ac43c41355e15439a610bf2f4d5404479
Maxim Tiourin <mixmaxtwo@gmail.com>
2017-09-29 16:48:54 -0700

    Added use of command line arguments to specify path to replay, began testing hero stat tracking

--- Heroes.ReplayParser.ConsoleApplication/ConsoleApplication1/Program.cs
+++ Heroes.ReplayParser.ConsoleApplication/ConsoleApplication1/Program.cs
@@ -12,6 +12,26 @@ using System.Text;
  */
 namespace ConsoleApplication1
 {
+    class HeroStats {
+        public string Name;
+        public int Kills;
+        public int Deaths;
+
+        public HeroStats(string name) {
+            Name = name;
+            Kills = 0;
+            Deaths = 0;
+        }
+
+        public void addKill() {
+            Kills++;
+        }
+
+        public void addDeath() {
+            Deaths++;
+        }
+    }
+
     class Program
     {
         private static StringBuilder sb = new StringBuilder();
@@ -25,186 +45,245 @@ namespace ConsoleApplication1
         private static char e = '"';
         private static char k = ':';
 
+        /*
+         * Args: executionPath filepathToReplay
+         */
         static void Main(string[] args)
         {
-            var heroesAccountsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"Heroes of the Storm\Accounts");
-            var randomReplayFileName = Directory.GetFiles(heroesAccountsFolder, "*.StormReplay", SearchOption.AllDirectories).OrderBy(i => Guid.NewGuid()).First();
+            int argc = args.Length;
+            int argexpect = (isWindowsOS()) ? (1) : (2);
 
-            // Use temp directory for MpqLib directory permissions requirements
-            var tmpPath = Path.GetTempFileName();
-            File.Copy(randomReplayFileName, tmpPath, overwrite: true);
+            if (argc != argexpect) {
+                Console.WriteLine("{\"error\": \"incorrect amount of arguments expect " + argexpect + "\"}");
+                return;
+            }
 
-            try
-            {
-                // Attempt to parse the replay
-                // Ignore errors can be set to true if you want to attempt to parse currently unsupported replays, such as 'VS AI' or 'PTR Region' replays
-                var replayParseResult = DataParser.ParseReplay(tmpPath, ignoreErrors: false, deleteFile: false);
+            string filepath = args[argexpect - 1];
 
-                // If successful, the Replay object now has all currently available information
-                if (replayParseResult.Item1 == DataParser.ReplayParseResult.Success) {
-                    var replay = replayParseResult.Item2;
+            if (File.Exists(filepath)) {
+                //var heroesAccountsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"Heroes of the Storm\Accounts");
+                //var randomReplayFileName = Directory.GetFiles(heroesAccountsFolder, "*.StormReplay", SearchOption.AllDirectories).OrderBy(i => Guid.NewGuid()).First();
 
-                    const int TEAMS = 2;
+                // Use temp directory for MpqLib directory permissions requirements
+                var tmpPath = Path.GetTempFileName();
+                File.Copy(filepath, tmpPath, overwrite: true);
 
-                    bool isRegionSet = false;
-                    bool isWinnerSet = false;
-                    int region = 1;
-                    int winner = 0;
+                try {
+                    // Attempt to parse the replay
+                    // Ignore errors can be set to true if you want to attempt to parse currently unsupported replays, such as 'VS AI' or 'PTR Region' replays
+                    var replayParseResult = DataParser.ParseReplay(tmpPath, ignoreErrors: false, deleteFile: false);
 
-                    //Begin JSON
-                    s(startObj());
+                    // If successful, the Replay object now has all currently available information
+                    if (replayParseResult.Item1 == DataParser.ReplayParseResult.Success) {
+                        var replay = replayParseResult.Item2;
 
-                    //Game Mode
-                    s(keystr("type", getGameMode(replay.GameMode)));
+                        const int TEAMS = 2;
 
-                    //Date "2017-08-27 13:43:09" "yyyy-mm-dd hh:mm:ss"
-                    s(keystr("date", getDateFormat(replay.Timestamp)));
+                        bool isRegionSet = false;
+                        bool isWinnerSet = false;
+                        int region = 1;
+                        int winner = 0;
 
-                    //Match Length
-                    s(keynum("match_length", (int) Math.Ceiling(replay.ReplayLength.TotalSeconds)));
+                        Dictionary<string, HeroStats> herostats = new Dictionary<string, HeroStats>();
 
-                    //Map
-                    s(keystr("map", replay.Map));
-
-                    //Version
-                    s(keystr("version", getVersionFormat(replay.ReplayVersion, replay.ReplayBuild)));
-
-                    //Players
-                    int playerCount = replay.Players.Length;
-                    int p = 0;
-                    s(startArr("players"));
-                    foreach (var player in replay.Players.OrderByDescending(i => i.IsWinner)) {
-                        //Start Player
+                        //Begin JSON
                         s(startObj());
 
-                        if (!isRegionSet) {
-                            region = player.BattleNetRegionId;
-                        }
-                        if (!isWinnerSet) {
-                            if (player.IsWinner) {
-                                winner = player.Team;
-                            }
-                        }
+                        //Game Mode
+                        s(keystr("type", getGameMode(replay.GameMode)));
 
-                        //Blizz_id
-                        s(keynum("id", player.BattleNetId));
+                        //Date "2017-08-27 13:43:09" "yyyy-mm-dd hh:mm:ss"
+                        s(keystr("date", getDateFormat(replay.Timestamp)));
 
-                        //Blizz_name
-                        s(keystr("name", player.Name));
+                        //Match Length
+                        s(keynum("match_length", (int) Math.Ceiling(replay.ReplayLength.TotalSeconds)));
 
-                        //Blizz_tag
-                        s(keynum("tag", player.BattleTag));
+                        //Map
+                        s(keystr("map", replay.Map));
 
-                        //Team
-                        s(keynum("team", player.Team));
+                        //Version
+                        s(keystr("version", getVersionFormat(replay.ReplayVersion, replay.ReplayBuild)));
 
-                        //Hero
-                        s(keystr("hero", player.Character));
-
-                        //Hero Level
-                        s(keynum("hero_level", player.CharacterLevel));
-
-                        //Talents
-                        int talentCount = player.Talents.Length;
-                        int t = 0;
-                        s(startArr("talents"));
-                        foreach (var talent in player.Talents.OrderBy(j => j.TalentID)) {
-                            //Id : Indexing for a hero starts at 0, and then increments top down, 
-                            //     left right, as moving through talents
-                            s(talent.TalentID + "");
-
-                            s(seperate(t < talentCount - 1));
-
-                            t++;
-                        }
-                        s(endArr(false)); //No comma termination for talents object
-
-                        //End Player (Make sure not to add comma after last player object)
-                        s(endObj(p < playerCount - 1));
-
-                        p++;
-                    }
-                    s(endArr());
-
-                    //Hero bans
-                    s(startObj("bans"));
-
-                    for (int team = 0; team < TEAMS; team++) {
-                        s(startArr(team + ""));
-
-                        if (replay.TeamHeroBans[team].Length > 0) {
-                            List<string> bans = replay.TeamHeroBans[team].Where(j => j != null && j.Length > 0).ToList();
-
-                            int banCount = bans.Count;
-                            int bx = 0;
-                            foreach (var b in bans) {
-                                s(str(b));
-
-                                s(seperate(bx < banCount - 1));
-
-                                bx++;
-                            }
-                        }
-
-                        s(endArr(team < TEAMS - 1));
-                    }
-
-                    s(endObj());
-
-                    //Team Xp sample points
-                    s(startObj("exp_samples"));
-
-                    for (int team = 0; team < TEAMS; team++) {
-                        s(startArr(team + ""));
-
-                        int xpCount = replay.TeamPeriodicXPBreakdown[team].Count;
-                        int x = 0;
-                        foreach (var exp in replay.TeamPeriodicXPBreakdown[team]) {
+                        //Players
+                        int playerCount = replay.Players.Length;
+                        int p = 0;
+                        s(startArr("players"));
+                        foreach (var player in replay.Players.OrderByDescending(i => i.IsWinner)) {
+                            //Start Player
                             s(startObj());
 
-                            //Time
-                            s(keynum("time", (int) Math.Ceiling(exp.TimeSpan.TotalSeconds)));
+                            if (!isRegionSet) {
+                                region = player.BattleNetRegionId;
+                            }
+                            if (!isWinnerSet) {
+                                if (player.IsWinner) {
+                                    winner = player.Team;
+                                }
+                            }
 
-                            //Team Level
-                            s(keynum("level", exp.TeamLevel));
+                            //Blizz_id
+                            s(keynum("id", player.BattleNetId));
 
-                            //Team Exp
-                            s(keynum("exp", exp.TotalXP, false));
+                            //Blizz_name
+                            s(keystr("name", player.Name));
 
-                            s(endObj(x < xpCount - 1));
+                            //Blizz_tag
+                            s(keynum("tag", player.BattleTag));
 
-                            x++;
+                            //Team
+                            s(keynum("team", player.Team));
+
+                            //Hero
+                            herostats[player.Character] = new HeroStats(player.Character);
+                            s(keystr("hero", player.Character));
+
+                            //Hero Level
+                            s(keynum("hero_level", player.CharacterLevel));
+
+                            //Talents
+                            int talentCount = player.Talents.Length;
+                            int t = 0;
+                            s(startArr("talents"));
+                            foreach (var talent in player.Talents.OrderBy(j => j.TalentID)) {
+                                //Id : Indexing for a hero starts at 0, and then increments top down, 
+                                //     left right, as moving through talents
+                                s(talent.TalentID + "");
+
+                                s(seperate(t < talentCount - 1));
+
+                                t++;
+                            }
+                            s(endArr(false)); //No comma termination for talents object
+
+                            //End Player (Make sure not to add comma after last player object)
+                            s(endObj(p < playerCount - 1));
+
+                            p++;
+                        }
+                        s(endArr());
+
+                        //Hero bans
+                        s(startObj("bans"));
+
+                        for (int team = 0; team < TEAMS; team++) {
+                            s(startArr(team + ""));
+
+                            if (replay.TeamHeroBans[team].Length > 0) {
+                                List<string> bans = replay.TeamHeroBans[team].Where(j => j != null && j.Length > 0).ToList();
+
+                                int banCount = bans.Count;
+                                int bx = 0;
+                                foreach (var b in bans) {
+                                    s(str(b));
+
+                                    s(seperate(bx < banCount - 1));
+
+                                    bx++;
+                                }
+                            }
+
+                            s(endArr(team < TEAMS - 1));
                         }
 
-                        s(endArr(team < TEAMS - 1));
+                        s(endObj());
+
+                        //Team Xp sample points
+                        s(startObj("exp_samples"));
+
+                        for (int team = 0; team < TEAMS; team++) {
+                            s(startArr(team + ""));
+
+                            int xpCount = replay.TeamPeriodicXPBreakdown[team].Count;
+                            int x = 0;
+                            foreach (var exp in replay.TeamPeriodicXPBreakdown[team]) {
+                                s(startObj());
+
+                                //Time
+                                s(keynum("time", (int) Math.Ceiling(exp.TimeSpan.TotalSeconds)));
+
+                                //Team Level
+                                s(keynum("level", exp.TeamLevel));
+
+                                //Team Exp
+                                s(keynum("exp", exp.TotalXP, false));
+
+                                s(endObj(x < xpCount - 1));
+
+                                x++;
+                            }
+
+                            s(endArr(team < TEAMS - 1));
+                        }
+
+                        s(endObj());
+
+                        //HeroStats
+                        List<Unit> heroUnits = replay.Units.Where(unit => unit.Group == Unit.UnitGroup.Hero).ToList();
+
+                        foreach (var unit in heroUnits) {
+                            string heroname = unit.PlayerControlledBy.Character;
+
+                            HeroStats stats = herostats[heroname];
+                            stats.addDeath();
+
+                            Player killerplayer = unit.PlayerKilledBy;
+                            if (killerplayer != null) {
+                                if (killerplayer.PlayerType == PlayerType.Human) {
+                                    string killerhero = unit.PlayerKilledBy.Character;
+                                    HeroStats killerStats = herostats[killerhero];
+                                    killerStats.addKill();
+                                }
+                            }
+                        }
+
+
+                        int heroesCount = herostats.Values.Count;
+                        int h = 0;
+                        s(startObj("hero_stats"));
+
+                        foreach (var hero in herostats.Values) {
+                            s(startObj(hero.Name));
+
+                            //Kills
+                            s(keynum("k", hero.Kills));
+
+                            //Deaths
+                            s(keynum("d", hero.Deaths, false));
+
+                            s(endObj(h < heroesCount - 1));
+                            
+                            h++;
+                        }
+
+                        s(endObj());
+
+                        //Region
+                        s(keynum("region", region));
+
+                        //Winner
+                        s(keynum("winner", winner, false));
+
+                        s(endObj(false));
+
+                        //TODO DEBUG file output
+                        System.IO.File.WriteAllText(@"test/output.json", sb.ToString());
+
+                        //Output JSON
+                        //Console.WriteLine(sb);
+
+                        //Console.Read(); // TODO DEBUG just to halt closing of cmdline window, use only for testing
                     }
-
-                    s(endObj());
-
-                    //Region
-                    s(keynum("region", region));
-
-                    //Winner
-                    s(keynum("winner", winner, false));
-
-                    s(endObj(false));
-
-                    //TODO DEBUG file output
-                    System.IO.File.WriteAllText(@"output.json", sb.ToString());
-
-                    //TODO DEBUG cmd output
-                    //Console.WriteLine(sb);
-
-                    //Console.Read(); // TODO DEBUG just to halt closing of cmdline window, use only for testing
+                    else {
+                        Console.WriteLine("{\"error\": \"replay can't be parsed\"}");
+                    }
                 }
-                else {
-                    Console.Write("error");
+                finally {
+                    if (File.Exists(tmpPath))
+                        File.Delete(tmpPath);
                 }
             }
-            finally
-            {
-                if (File.Exists(tmpPath))
-                    File.Delete(tmpPath);
+            else {
+                Console.WriteLine("{\"error\": \"no file at given path\"}");
             }
         }
 
@@ -292,6 +371,21 @@ namespace ConsoleApplication1
 
         private static string getVersionFormat(string v, int build) {
             return v.Substring(2) + "." + build;
+        }
+
+        private static bool isWindowsOS() {
+            switch (Environment.OSVersion.Platform) {
+                case PlatformID.WinCE:
+                    return true;
+                case PlatformID.Win32Windows:
+                    return true;
+                case PlatformID.Win32S:
+                    return true;
+                case PlatformID.Win32NT:
+                    return true;
+                default:
+                    return false;
+            }
         }
 
         private static byte[] GetMpqArchiveFileBytes(MpqArchive archive, string fileName)
